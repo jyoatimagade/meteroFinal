102834e196edd98efa60a9211b2bc71972e89faa
Saurabh Jain <saurabhs.jain4u@gmail.com>
2021-07-04 15:27:38 +0530

    if not loggedin then redirect to login screen done

--- src/_screens/_landingPage/Metero.js
+++ src/_screens/_landingPage/Metero.js
@@ -3,10 +3,15 @@ import { Header, HeaderBottom, Footer } from "../../_components";
 import MeteroTab from "./MeteroTab";
 import { ThemeProvider } from "styled-components";
 import {SplashScreen} from   "../../_components/_UICompoents/SplashScreen"
+import { Redirect } from "react-router-dom";
 
 const Metero = () => {
   const [selectedJob, setSelectedJob] = useState("");
  
+  if (!sessionStorage.getItem("UserId")) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <>
       {/* <ThemeProvider theme={themes[theme]}> */}
