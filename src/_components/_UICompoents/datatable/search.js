7f265af968b20556f84e487377454b1024ea0f18
Saurabh Jain <saurabhs.jain4u@gmail.com>
2021-07-06 01:48:34 +0530

    close icon added for search

--- src/_components/_UICompoents/datatable/search.js
+++ src/_components/_UICompoents/datatable/search.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-const Search = ({onSearch}) =>{
+import { closeIcon } from "../../../_config/images";
+
+const Search = ({showClose, onSearch}) =>{
     const [search, setSearch] = useState('');
     const onInputChange = (value)=>{
         setSearch(value);
@@ -7,11 +9,18 @@ const Search = ({onSearch}) =>{
     }
     return(
         <>
+        <div className="search-container" style={{position:'relative'}}>
        <input type="text"
        className="form-control"
        placeholder="Search"
        onChange={(e)=>onInputChange(e.target.value)}
+       value={search}
+       style={{
+           paddingRight: showClose && search.length ? '30px !important' : '12px !important'
+       }}
        />
+       { showClose && search.length ? <img onClick={()=>{setSearch('');onSearch('')}} style={{position:'absolute',top:'50%',right:'10px',transform:'translateY(-50%)',cursor:'pointer'}} src={closeIcon} /> : ''}
+       </div>
         </>
     )
 }
