9302d998d07f5f2df80872f9808fbb027f4b4207
Saurabh Jain <saurabhs.jain4u@gmail.com>
2021-07-08 02:38:45 +0530

    search on back press handled

--- src/_components/_UICompoents/datatable/search.js
+++ src/_components/_UICompoents/datatable/search.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react';
 import { searchcloseIcon } from "../../../_config/images";
 
-const Search = ({showClose, onSearch}) =>{
+const Search = ({showClose, onSearch, onBackPress}) =>{
+    let searchTerm = "";
     const [search, setSearch] = useState('');
     const onInputChange = (value)=>{
+        searchTerm = value;
         setSearch(value);
         onSearch(value);
+    }
+    const onBack = (keyCode) => {
+        setTimeout(() => {
+            if(keyCode !== 8) return; 
+            // console.log(searchTerm);
+            onBackPress(searchTerm);
+        }, 50);
     }
     return(
         <>
@@ -14,6 +23,7 @@ const Search = ({showClose, onSearch}) =>{
        className="form-control"
        placeholder="Search"
        onChange={(e)=>onInputChange(e.target.value)}
+       onKeyDown={(e)=>onBack(e.keyCode)}
        value={search}
        style={{
            paddingRight: showClose && search.length ? '30px !important' : '12px !important'
